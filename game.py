c2ff92386bb58d2d4c41d8655d393b2c993cfddb
Kira Hartlage <kira.hartlage@gmail.com>
2020-05-23 15:32:37 -0400

    continuously ask to play again until quit

--- game.py
+++ game.py
@@ -12,44 +12,34 @@ random.shuffle(alphabet)
 key = {original_alphabet[i]: alphabet[i]
        for i in range(len(original_alphabet))}
 
-
-def get_start():
-    response = input("Do you want to play? (y/n) ")
-    if response.lower() == "y":
-        return "Let's play!"
-    else:
-        return "See you later!"
-
-
-def get_puzzle():
-    transformed_puzzle = "     ".join(puzzle.split())
-    transformed_puzzle = re.sub("[a-zA-Z]", "_ ", transformed_puzzle)
-    return transformed_puzzle
-
-
-def play_game():
-    print(get_start())
-    print(get_puzzle())
-    print()
-
-
 class Game:
 
     def __init__(self):
         pass
 
-    def ask_user(self):
-        response = input("Do you want to play? (y/n) ")
-        if response.lower() == "y":
-            return "Let's play!"
+    def ask_user_to_play(self):
+        response = input("Do you want to play? (y/n) ").lower()
+        if response == "y":
+            print("Let's play!")
         else:
-            return "See you later!"
+            print("See you later!")
+        return response
 
     def get_puzzle(self):
         transformed_puzzle = "     ".join(puzzle.split())
         transformed_puzzle = re.sub("[a-zA-Z]", "_ ", transformed_puzzle)
-        return transformed_puzzle
+        print(transformed_puzzle)
+
+    def play_round(self):
+        self.get_puzzle()
+
+    def play_game(self):
+        play_again = None
+        while play_again != 'n':
+            self.play_round()
+            play_again = self.ask_user_to_play()
 
 
 if __name__ == '__main__':
-    play_game()
+    game = Game()
+    game.play_game()
