d93ebcc3a04ebca8eb938e7e11ce122aea5f8cb5
Kira Hartlage <kira.hartlage@gmail.com>
2020-05-24 09:34:27 -0400

    display puzzle

--- puzzle.py
+++ puzzle.py
@@ -5,7 +5,7 @@ puzzle = "I am going to play this game"
 
 alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
             'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-            'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 original_alphabet = list.copy(alphabet)
 random.shuffle(alphabet)
 
@@ -14,18 +14,20 @@ key = {original_alphabet[i]: alphabet[i]
 
 guesses = {original_alphabet[i]: '' for i in range(len(original_alphabet))}
 
-print(f'key is {key}')
-print(f'guesses are {guesses}')
+# print(f'key is {key}')
+# print(f'guesses are {guesses}')
 
 guesses['A'] = 'B'
 
-print(f'guesses are {guesses}')
+# print(f'guesses are {guesses}')
+
 
 def are_guesses_correct(key, guesses):
     return key == guesses
 
-print(are_guesses_correct(key, guesses))
-print(are_guesses_correct(key, key))
+# print(are_guesses_correct(key, guesses))
+# print(are_guesses_correct(key, key))
+
 
 hashed_puzzle = ""
 
@@ -40,5 +42,51 @@ transformed_puzzle = re.sub("[a-zA-Z]", "_", transformed_puzzle)
 
 hashed_puzzle = "   ".join(hashed_puzzle.split())
 
-print(transformed_puzzle)
-print(hashed_puzzle)
+# print(transformed_puzzle)
+# print(hashed_puzzle)
+
+
+class Puzzle:
+    ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    PUZZLES = ["I am going to play this game",
+               "I am going to win this game", "You are going to lose"]
+
+    def __init__(self):
+        self.key = dict()
+        self.guesses = {Puzzle.ALPHABET[i]                        : '' for i in range(len(Puzzle.ALPHABET))}
+        self.puzzle = ""
+
+    def get_key(self):
+        original_alphabet = list.copy(Puzzle.ALPHABET)
+        random.shuffle(original_alphabet)
+        self.key = {Puzzle.ALPHABET[i]: original_alphabet[i]
+                    for i in range(len(Puzzle.ALPHABET))}
+        # print(self.key)
+
+    def get_puzzle(self):
+        self.puzzle = random.choice(Puzzle.PUZZLES)
+        # print(self.puzzle)
+
+    def display_puzzle(self):
+        hashed_puzzle = ""
+        for letter in self.puzzle:
+            if letter.isspace():
+                hashed_puzzle += letter
+            else:
+                hashed_puzzle += self.key[letter.upper()]
+
+        transformed_puzzle = "   ".join(self.puzzle.split())
+        transformed_puzzle = re.sub("[a-zA-Z]", "_", transformed_puzzle)
+
+        hashed_puzzle = "   ".join(hashed_puzzle.split())
+
+        print(transformed_puzzle)
+        print(hashed_puzzle)
+
+puzzle = Puzzle()
+puzzle.get_key()
+puzzle.get_puzzle()
+puzzle.display_puzzle()
