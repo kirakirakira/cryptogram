b8820f8d2c914dc0c2c5aea7a82323d617571dca
Kira Hartlage <kira.hartlage@gmail.com>
2020-05-25 16:10:27 -0400

    added more trump tweets and started cleaning them up for the game

--- trumpograms/trumpdump.py
+++ trumpograms/trumpdump.py
@@ -9,13 +9,25 @@ tweets = dict({})
 re.sub("[a-zA-Z]", "_", "abcdefghiject")
 
 for index, tweet in enumerate(trump_dump, start=1):
-    # tweets[index] = "     ".join(tweet['text'].split())
-    # tweets[index] = re.sub("[a-zA-Z]", "_ ", tweets[index])
     tweets[index] = tweet['text']
     tweets[index] = re.sub(r"http\S+", '', tweets[index])
-    print(index)
-    print(tweets[index])
-    print("--"*8)
+    # print(index)
+    # print(tweets[index])
+    # print("--"*8)
 
+# keys_to_delete = []
+
+# for key, value in tweets.items():
+#     if value.startswith('RT'):
+#         keys_to_delete.append(key)
+
+# for key in keys_to_delete:
+#     del tweets[key]
+
+new_tweets = { k:v for k, v in tweets.items() if not v.startswith('RT')}
+
+for key, value in new_tweets.items():
+    print(key)
+    print(value)
 
 f.close()
