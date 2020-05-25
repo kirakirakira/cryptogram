6716c89f8df032ad703a638a93d2b1c6aac76f7f
Kira Hartlage <kira.hartlage@gmail.com>
2020-05-25 16:15:06 -0400

    remove ones that start with https too, they're usually only links

--- trumpograms/trumpdump.py
+++ trumpograms/trumpdump.py
@@ -10,7 +10,7 @@ re.sub("[a-zA-Z]", "_", "abcdefghiject")
 
 for index, tweet in enumerate(trump_dump, start=1):
     tweets[index] = tweet['text']
-    tweets[index] = re.sub(r"http\S+", '', tweets[index])
+    # tweets[index] = re.sub(r"http\S+", '', tweets[index])
     # print(index)
     # print(tweets[index])
     # print("--"*8)
@@ -25,6 +25,7 @@ for index, tweet in enumerate(trump_dump, start=1):
 #     del tweets[key]
 
 new_tweets = { k:v for k, v in tweets.items() if not v.startswith('RT')}
+new_tweets = { k:v for k, v in new_tweets.items() if not v.startswith('https')}
 
 for key, value in new_tweets.items():
     print(key)
