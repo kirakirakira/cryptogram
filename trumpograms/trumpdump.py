db54c8542d94f5e61cc972125ab8b27d36e99268
Kira Hartlage <kira.hartlage@gmail.com>
2020-05-26 18:50:38 -0400

    clean up tweets more

--- trumpograms/trumpdump.py
+++ trumpograms/trumpdump.py
@@ -10,22 +10,13 @@ re.sub("[a-zA-Z]", "_", "abcdefghiject")
 
 for index, tweet in enumerate(trump_dump, start=1):
     tweets[index] = tweet['text']
-    # tweets[index] = re.sub(r"http\S+", '', tweets[index])
-    # print(index)
-    # print(tweets[index])
-    # print("--"*8)
-
-# keys_to_delete = []
-
-# for key, value in tweets.items():
-#     if value.startswith('RT'):
-#         keys_to_delete.append(key)
-
-# for key in keys_to_delete:
-#     del tweets[key]
 
 new_tweets = { k:v for k, v in tweets.items() if not v.startswith('RT')}
 new_tweets = { k:v for k, v in new_tweets.items() if not v.startswith('https')}
+
+for index, tweet in new_tweets.items():
+    new_tweets[index] = re.sub(r"http\S+", '', new_tweets[index])
+    new_tweets[index] = re.sub(r"@\S+", '@_%&*#$%^', new_tweets[index])
 
 for key, value in new_tweets.items():
     print(key)
